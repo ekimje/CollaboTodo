c7b222cde4227164fd54c17dae4aaa7099e4ae4e
김지윤 <blue201232@gmail.com>
2026-05-07 21:28:51 +0900

    스프링부트 설치

---
+++ CollaboTodo/src/main/java/CollaboTodo/CollaboTodoApplication.java
@@ -0,0 +1,14 @@
+package CollaboTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
+
+@SpringBootApplication
+public class CollaboTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaboTodoApplication.class, args);
+	}
+
+}
